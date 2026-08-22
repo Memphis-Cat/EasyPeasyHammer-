75d0d726eef305706393aee7ea8eba7a5db247b0
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 18:37:09 +0100

    Ensure Phase 3 dependencies install before build

--- Build_EXE.bat
+++ Build_EXE.bat
@@ -3,11 +3,16 @@
 setlocal
 cd /d "%~dp0"
 
-if not exist node_modules (
-    call npm install
-    if errorlevel 1 exit /b 1
-)
+if not exist node_modules\electron\dist\electron.exe goto install
+if not exist node_modules\three\build\three.module.js goto install
+goto build
 
+:install
+echo Installing build dependencies...
+call npm install
+if errorlevel 1 exit /b 1
+
+:build
 call npm run build
 if errorlevel 1 exit /b 1
 
