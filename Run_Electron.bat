f943ec397305679a2c2be9a8744803ea11550df2
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 19:13:46 +0100

    Pause Electron runner on success and errors

--- Run_Electron.bat
+++ Run_Electron.bat
@@ -2,6 +2,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+set "EXIT_CODE=0"
 
 if not exist node_modules\electron\dist\electron.exe goto installnode
 if not exist node_modules\three\build\three.module.js goto installnode
@@ -14,8 +15,11 @@ if errorlevel 1 goto error
 
 :backend
 if exist "backend\EasyPeasyHammer.AssetHost\bin\Release\net10.0\win-x64\publish\EasyPeasyHammer.AssetHost.exe" goto run
+set "EPH_NO_PAUSE=1"
 call Build_Backend.bat
-if errorlevel 1 goto error
+set "BACKEND_CODE=%ERRORLEVEL%"
+set "EPH_NO_PAUSE="
+if not "%BACKEND_CODE%"=="0" goto error
 
 :run
 call npm start
@@ -26,11 +30,11 @@ echo EasyPeasyHammer closed normally.
 goto done
 
 :error
+set "EXIT_CODE=1"
 echo.
 echo EasyPeasyHammer stopped because of an error. Check the output above.
 
-echo.
 :done
+echo.
 pause
-endlocal
-exit /b
+endlocal & exit /b %EXIT_CODE%
