366b998bd91de6d4fde76d1888807a660f8165a4
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 19:13:54 +0100

    Pause EXE build on success and errors

--- Build_EXE.bat
+++ Build_EXE.bat
@@ -2,22 +2,31 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+set "EXIT_CODE=0"
 
+set "EPH_NO_PAUSE=1"
 call Build_Backend.bat
-if errorlevel 1 exit /b 1
+set "BACKEND_CODE=%ERRORLEVEL%"
+set "EPH_NO_PAUSE="
+if not "%BACKEND_CODE%"=="0" goto error
 
-if not exist node_modules (
-    call npm install
-    if errorlevel 1 exit /b 1
-) else (
-    call npm install
-    if errorlevel 1 exit /b 1
-)
+echo Installing/updating Electron dependencies...
+call npm install
+if errorlevel 1 goto error
 
 call npm run build
-if errorlevel 1 exit /b 1
+if errorlevel 1 goto error
 
 echo.
 echo Build complete. Check the dist folder for the installer and portable .exe.
+goto done
+
+:error
+set "EXIT_CODE=1"
+echo.
+echo EXE build stopped because of an error. Check the output above.
+
+:done
+echo.
 pause
-endlocal
+endlocal & exit /b %EXIT_CODE%
