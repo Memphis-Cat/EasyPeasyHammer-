42eba414667dc7da3625aba6a50a7fe08d03dc0d
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 19:00:50 +0100

    Build backend into Windows Electron packages

--- Build_EXE.bat
+++ Build_EXE.bat
@@ -3,20 +3,21 @@
 setlocal
 cd /d "%~dp0"
 
-if not exist node_modules\electron\dist\electron.exe goto install
-if not exist node_modules\three\build\three.module.js goto install
-goto build
-
-:install
-echo Installing build dependencies...
-call npm install
+call Build_Backend.bat
 if errorlevel 1 exit /b 1
 
-:build
+if not exist node_modules (
+    call npm install
+    if errorlevel 1 exit /b 1
+) else (
+    call npm install
+    if errorlevel 1 exit /b 1
+)
+
 call npm run build
 if errorlevel 1 exit /b 1
 
 echo.
-echo Build complete. Check the dist folder.
+echo Build complete. Check the dist folder for the installer and portable .exe.
 pause
 endlocal
