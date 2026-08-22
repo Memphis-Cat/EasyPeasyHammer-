a4454383dc8f6a34104e150942da0bb574281acb
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 22:21:43 +0100

    Load fidelity v2 fixes

--- src/project-dialog.js
+++ src/project-dialog.js
@@ -147,6 +147,7 @@ function loadPass(src, marker, module = false) {
     await loadPass('interaction-pass.js', 'ephInteractionPass');
     await loadPass('advanced-viewport.js', 'ephAdvancedViewport', true);
     await loadPass('hammer-fidelity.js', 'ephHammerFidelity', true);
+    await loadPass('fidelity-v2.js', 'ephFidelityV2', true);
     await loadPass('default-part.js', 'ephDefaultPart');
     await loadPass('hammer-fidelity-ui.js', 'ephHammerFidelityUi');
     await loadPass('advanced-ui.js', 'ephAdvancedUi');
