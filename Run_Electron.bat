e272c3b19815c12e0ef8326470094af531554389
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 18:37:04 +0100

    Ensure Phase 3 dependencies install before launch

--- Run_Electron.bat
+++ Run_Electron.bat
@@ -3,11 +3,15 @@
 setlocal
 cd /d "%~dp0"
 
-if not exist node_modules\electron\dist\electron.exe (
-    echo Installing Electron dependencies...
-    call npm install
-    if errorlevel 1 exit /b 1
-)
+if not exist node_modules\electron\dist\electron.exe goto install
+if not exist node_modules\three\build\three.module.js goto install
+goto run
 
+:install
+echo Installing Electron dependencies...
+call npm install
+if errorlevel 1 exit /b 1
+
+:run
 call npm start
 endlocal
