18f4b03bc725450ca2612bcc28dfd5d01ae2adb5
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-22 19:00:42 +0100

    Run Phase 3 asset backend with Electron

--- Run_Electron.bat
+++ Run_Electron.bat
@@ -3,13 +3,18 @@
 setlocal
 cd /d "%~dp0"
 
-if not exist node_modules\electron\dist\electron.exe goto install
-if not exist node_modules\three\build\three.module.js goto install
-goto run
+if not exist node_modules\electron\dist\electron.exe goto installnode
+if not exist node_modules\three\build\three.module.js goto installnode
+goto backend
 
-:install
+:installnode
 echo Installing Electron dependencies...
 call npm install
+if errorlevel 1 exit /b 1
+
+:backend
+if exist "backend\EasyPeasyHammer.AssetHost\bin\Release\net10.0\win-x64\publish\EasyPeasyHammer.AssetHost.exe" goto run
+call Build_Backend.bat
 if errorlevel 1 exit /b 1
 
 :run
