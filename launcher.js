30751d1b9410493af4b45d6059aaa7cdc3f6fd38
Memphis-Cat <MemphisCatRBetter.en-us.pt@outlook.com>
2026-08-23 01:42:53 +0100

    Keep collaboration notifications active in background

--- launcher.js
+++ launcher.js
@@ -2,6 +2,9 @@
 const electron = require('electron');
 const ws = require('ws');
 
+electron.app.commandLine.appendSwitch('disable-renderer-backgrounding');
+electron.app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+
 const NativeWebSocketServer = ws.WebSocketServer;
 ws.WebSocketServer = class EasyPeasyHammerWebSocketServer extends NativeWebSocketServer {
   constructor(options = {}, callback) {
